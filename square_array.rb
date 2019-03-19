e7fe4f523adc53070c15d8144379386fda15697d
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-19 18:02:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+def square_array(number)
+  numbers = [1, 2, 3, 4]
+  
+  numbers.each do |n|
+    sq_ans = #{n}*#{n}
+    puts sq_ans
+  end
 end
+
+  square_array(number)
